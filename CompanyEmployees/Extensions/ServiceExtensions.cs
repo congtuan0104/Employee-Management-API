eda43e7b45bd0e3401766dd39e02e4c3af9097ca
Công Tuấn (TiT) <congtuan0104@gmail.com>
2023-04-29 22:50:07 +0700

    Creating a Repository Manager

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using LoggerService;
+using Repository;
 
 namespace CompanyEmployees.Extensions
 {
@@ -33,6 +34,9 @@ namespace CompanyEmployees.Extensions
         
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddSingleton<ILoggerManager, LoggerManager>();
+        
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
         
     }
 }
